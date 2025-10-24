58521a778f64ce9ffbadb2b4a629bfd268248c94
Ahsan <sbaseer25@gmail.com>
2025-10-24 11:48:21 -0400

    setup the classes and methods

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private List<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public void addVehicle(Vehicle v) {
+        inventory.add(v);
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return new ArrayList<>(inventory);
+    }
+
+    //TODO:
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String type) {
+        return null;
+    }
+
+    public boolean removeVehicle(int vin) {
+        return false;
+    }
+
+}
